13b8f5ca798efd6833663a0a8d41037fe9d6286a
Arun Aenishaenslin <arun_aenishaenslin@sluz.ch>
2024-11-28 16:09:05 +0100

    28.11.2024

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -5,10 +5,17 @@ import "fmt"
 func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
+	// Direktes Befüllen bis Index 4
 	fibs[2] = fibs[0] + fibs[1]
+	fibs[3] = fibs[1] + fibs[2]
+	fibs[4] = fibs[2] + fibs[3]
 	// TODO: correct up to index 4 using direct element access
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
+	// Anhängen weiterer Fibonacci-Zahlen
+	fibs = append(fibs, fibs[3]+fibs[4]) // TODO: replace 0 with the next Fibonacci number
+	fibs = append(fibs, fibs[4]+fibs[5])
+	fibs = append(fibs, fibs[5]+fibs[6])
+	fibs = append(fibs, fibs[6]+fibs[7])
 	// TODO: compute three more Fibonacci numbers and append them
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,58 @@
 package main
 
+import "fmt"
+
 func main() {
 	// TODO: declare a type for Student (with first and last name)
+	type Student struct {
+		FirstName string
+		LastName  string
+	}
+
 	// TODO: declare a type for Class (consisting of multiple students)
+	type Class struct {
+		Name     string
+		Students []Student
+	}
+
 	// TODO: declare a map of modules being attended by multiple classes
+	modules := map[int][]Class{
+		346: {
+			{Name: "Class A", Students: []Student{
+				{FirstName: "Alice", LastName: "Smith"},
+				{FirstName: "Bob", LastName: "Johnson"},
+				{FirstName: "Charlie", LastName: "Brown"},
+			}},
+			{Name: "Class B", Students: []Student{
+				{FirstName: "David", LastName: "Wilson"},
+				{FirstName: "Eve", LastName: "Taylor"},
+				{FirstName: "Frank", LastName: "Anderson"},
+			}},
+		},
+		104: {
+			{Name: "Class A", Students: []Student{
+				{FirstName: "Alice", LastName: "Smith"},
+				{FirstName: "Bob", LastName: "Johnson"},
+				{FirstName: "Charlie", LastName: "Brown"},
+			}},
+		},
+		117: {
+			{Name: "Class B", Students: []Student{
+				{FirstName: "David", LastName: "Wilson"},
+				{FirstName: "Eve", LastName: "Taylor"},
+				{FirstName: "Frank", LastName: "Anderson"},
+			}},
+		},
+	}
+
 	// TODO: output everything using fmt.Println()
+	for moduleID, classes := range modules {
+		fmt.Printf("Module %d:\n\n", moduleID)
+		for _, class := range classes {
+			fmt.Printf("  Class %s:\n\n", class.Name)
+			for _, student := range class.Students {
+				fmt.Printf("    - %s %s\n", student.FirstName, student.LastName)
+			}
+		}
+	}
 }
